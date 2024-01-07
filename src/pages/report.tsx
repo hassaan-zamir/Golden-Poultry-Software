f76dbae57bd4eaf32ed5d4e8495498b6b4f3416f
hassaan-zamir <hassaanzamir94@gmail.com>
2024-01-07 18:24:57 +0500

    dsr

--- src/pages/report.tsx
+++ src/pages/report.tsx
@@ -1,20 +1,24 @@
 import Head from "next/head";
 import axios from "axios";
-import { useEffect, useState } from "react";
-
+import { useRef, useState } from "react";
+import { useReactToPrint } from "react-to-print";
+import { toast } from "react-toastify";
 
 export async function getServerSideProps() {
   const res = await axios.post(
     process.env.NEXT_PUBLIC_BASE_URL + "/api/getMasterData"
   );
-  const allInvoices = res.data.invoices;
-  console.log(allInvoices.length, 'hi');
+  const invoices = res.data.invoices;
+  const sheds:string[] = [];
 
+  invoices.map((inv: InvoiceType) => {
+    if (!sheds.includes(inv.shed)){
+      sheds.push(inv.shed)
+    }
+  })
 
-  return { props: { allInvoices: allInvoices } };
+  return { props: { invoices, sheds } };
 }
-
-
 
 export interface InvoiceType {
   add_less: number;
@@ -35,109 +39,92 @@ export interface InvoiceType {
 }
 
 interface PropTypes {
-  allInvoices: InvoiceType[];
+  invoices: InvoiceType[];
+  sheds: string[];
 }
 
+export default function Home({ invoices, sheds }: PropTypes) {
+  const componentRef = useRef(null);
 
-export default function Home({  allInvoices }: PropTypes) {
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+  });
 
-  const [date, setDate] = useState<string>((new Date()).toISOString().substr(0, 10));
-  const [invoices, setInvoices] = useState<InvoiceType[]>(allInvoices);
+  const [date, setDate] = useState<string>(
+    new Date().toISOString().substr(0, 10)
+  );
+  const [shed, setShed] = useState<string>(sheds[0] ?? "");
+  const [filteredInvoices, setFilteredInvoices] = useState<InvoiceType[]>([]);
 
-  useEffect(() => {
-    if(allInvoices?.length){
+  const fetchInvoices = () => {
+    setFilteredInvoices([]);
+    if (invoices?.length) {
       const cdate = new Date(date);
       const year = cdate.getFullYear();
-      const month = String(cdate.getMonth() + 1).padStart(2, '0');
-      const day = String(cdate.getDate()).padStart(2,'0');
+      const month = String(cdate.getMonth() + 1).padStart(2, "0");
+      const day = String(cdate.getDate()).padStart(2, "0");
 
-      let filteredInvoices = allInvoices.filter((inv:InvoiceType) => inv.date == `${year}-${month}-${day}`);
+      let filteredInvoices = invoices.filter(
+        (inv) => inv.date === `${year}-${month}-${day}` && inv.shed === shed
+      );
+      setFilteredInvoices(filteredInvoices);
 
-      setInvoices(filteredInvoices);
+      if (filteredInvoices.length > 0) {
+        toast.error("No invoices found");
+      }
+    } else {
+      toast.error("No Invoices Found");
     }
+  };
 
-
-  },[date])
-
-  function numberWithCommas(x:number) {
+  function numberWithCommas(x: number) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   }
 
-    const totalCash = ():number => {
-        let totalCash = 0;
-        invoices.map((invoice) => totalCash+= (invoice.cash))
-        return totalCash;
-    }
 
-    const totalOnline = ():number => {
-        let totalOnline = 0;
-        invoices.map((invoice) => totalOnline+= invoice.online)
-        return totalOnline;
-    }
-  
-    const totalAdvance = ():number => {
-        let totalAdvance = 0;
-        invoices.map((invoice) => totalAdvance+= getTotalAdvance(invoice));
-        return totalAdvance;
-    }
-
-    const totalnetwgt = ():number => {
-        let totalnetwgt = 0;
-        invoices.map((invoice) => totalnetwgt+= getNetWeight(invoice));
-        return totalnetwgt;
-    }
-
-    const totalamt = ():number => {
-        let totalamt = 0;
-        invoices.map((invoice) => totalamt+= getTotalAmount(invoice));
-        return totalamt;
-    }
-
-    const totalbalance = ():number => {
-       
-        let totalbalance = 0;
-        invoices.map((invoice) => {
-          // if(paidCheck) {
-          //   if(invoice.paid){
-          //     totalbalance+= getBalance(invoice);
-          //   }
-          // }else{
-            totalbalance+= getBalance(invoice);
-          // }
-        });
-        return totalbalance;
-    }
-
-    const totalcommission = ():number => {
-        let totalcommission = 0;
-        invoices.map((invoice) => totalcommission+= invoice.commission);
-        return totalcommission;
-    }
 
   const getFinalRate = (invoice: InvoiceType): number => {
     return invoice.todays_rate + invoice.add_less;
   };
 
-  const getTotalAdvance = (invoice:InvoiceType): number => {
-    return invoice.online+invoice.cash;
+  const getTotalAdvance = (invoice: InvoiceType): number => {
+    return invoice.online + invoice.cash;
   };
 
-  const getNetWeight = (invoice:InvoiceType): number => {
-    return invoice.second_weight-invoice.first_weight;
+  const getNetWeight = (invoices: InvoiceType[]): number => {
+    let sum = 0;
+    invoices.map((inv) => {
+      sum += (inv.second_weight - inv.first_weight);
+    })
+    return sum;
   };
 
-  const getTotalAmount = (invoice:InvoiceType): number => {
-    return getFinalRate(invoice) * getNetWeight(invoice);
+  const getTotalAmount = (invoices: InvoiceType[]): number => {
+    let sum = 0;
+    invoices.map((inv) => {
+      sum += getFinalRate(inv) * (inv.second_weight - inv.first_weight);
+    });
+    return sum;
   };
 
-  const getBalance = (invoice:InvoiceType): number => {
-    if(invoice.paid){
-      return 0;
-    }
-    return getTotalAmount(invoice) - getTotalAdvance(invoice);
+  const getBalance = (invoices: InvoiceType[]): number => {
+    let sum = 0;
+    invoices.map((inv) => {
+      if(!inv.paid){
+        sum+= ((getFinalRate(inv) * (inv.second_weight - inv.first_weight)) - getTotalAdvance(inv));
+      }
+    })
+
+    return sum;
   };
 
-
+  const getTotalCommission = (invoices: InvoiceType[]): number => {
+    let sum = 0;
+    invoices.map(inv => {
+      sum+= inv.commission;
+    }); 
+    return sum;
+  }
 
   return (
     <>
@@ -147,68 +134,148 @@ export default function Home({  allInvoices }: PropTypes) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div style={{ width: '100vw', background: '#fff', color: '#000'}}>
-      <section className="text-center">
+      <div style={{ width: "100vw", background: "#fff", color: "#000", textAlign: 'center' }}>
         <h1>Report</h1>
-        <input type="date" value={date} onChange={(e) => setDate(e.target.value)} />
-      </section>
+        <section
+          className="text-center"
+          style={{ display: "flex", justifyContent: "center", gap: "10px" }}
+        >
+          <input
+            type="date"
+            className="w-80"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
+          <br />
+          <select
+            name="shed_no"
+            className="w-80"
+            onChange={(e) => {
+              setShed(e.target.value);
+            }}
+            defaultValue={shed}
+          >
+            {sheds.map((shed, i) => (
+              <option key={i} value={shed}>
+                {shed}
+              </option>
+            ))}
+          </select>
+          <button onClick={fetchInvoices}>Submit</button>
+          {filteredInvoices.length > 0 && (
+            <button onClick={handlePrint}>Print</button>
+          )}
+        </section>
 
-      {invoices?.length && <section id="table">
-        <table cellSpacing={10} cellPadding={4} >
-            <thead>
-                <tr>
-                    <th>Driver Name</th>
-                    <th>Vehicle Number</th>
-                    <th>Cash</th>
-                    <th>Online</th>
-                    <th>Total Advance</th>
-                    <th>Net Wgt</th>
-                    <th>Total Amt</th>
-                    <th>Credit</th>
-                    <th>Comission</th>
-                </tr>
-            </thead>
+        <section style={{ background: "white", color: "black" }}>
+          <table
+            className="print-table"
+            ref={componentRef}
+            style={{
+              textAlign: "center",
+              background: "#fff",
+              color: "#000",
+              border: "none",
+            }}
+            border={0}
+            cellSpacing={10}
+          >
             <tbody>
-               
-                {invoices.map((invoice, i) => {
-                
-                return <tr key={i}>
-                    <td>{invoice.driver_name}</td>
-                    <td>{invoice.vehicle_no}</td>
-                    <td>{invoice.cash}</td>
-                    <td>{invoice.online}</td>
-                    <td>{getTotalAdvance(invoice)}</td>
-                    <td>{getNetWeight(invoice)}</td>
-                    <td>{getTotalAmount(invoice)}</td>
-                    <td>{getBalance(invoice)}</td>
-                    <td>{invoice.commission}</td>
-                </tr>}
-                )}
+              <tr>
+                <th colSpan={4}>
+                  <h2>
+                    GOLDEN POULTRY
+                    <br />
+                    FARMS
+                  </h2>
+                </th>
+              </tr>
 
-                <tr>
-                    <th colSpan={2}>Total</th>
-                   
-                    <th>{numberWithCommas(totalCash())}</th>
-                    <th>{numberWithCommas(totalOnline())}</th>
-                    <th>{numberWithCommas(totalAdvance())}</th>
-                    <th>{numberWithCommas(totalnetwgt())}</th>
-                    <th>{numberWithCommas(totalamt())}</th>
-                    <th>{numberWithCommas(totalbalance())}</th>
-                    <th>{numberWithCommas(totalcommission())}</th>
-                </tr>
+              <tr>
+                <th colSpan={4}>DSR</th>
+              </tr>
 
-                <tr>
-                    <th colSpan={5}>Net Cash</th>
-                    <th colSpan={5}>{numberWithCommas(totalamt() + totalcommission() - totalbalance() - totalOnline())}</th>
-       
-                </tr>
+              <tr>
+                <td colSpan={2}>
+                  <b>Date</b>
+                  <br />
+                  <span>{new Date(date).toISOString().substr(0, 10)}</span>
+                </td>
 
-              
+                <td colSpan={2}>
+                  <b>Shed</b>
+                  <br />
+                  <span>{shed}</span>
+                </td>
+              </tr>
 
+              <tr>
+                <td colSpan={2}>
+                  <b>Total Weight</b>
+            
+                </td>
+
+                <td colSpan={2}>
+                  <span>{numberWithCommas(getNetWeight(filteredInvoices))}</span>
+                </td>
+              </tr>
+
+              <tr>
+                <td colSpan={2}>
+                  <b>Total Sales</b>
+                </td>
+
+                <td colSpan={2}>
+                  <span>{numberWithCommas(getTotalAmount(filteredInvoices))}</span>
+                </td>
+              </tr>
+
+              <tr>
+                <td colSpan={2}>
+                  <b>Total Online</b>
+                </td>
+
+                <td colSpan={2}>
+                  <span>{numberWithCommas(getBalance(filteredInvoices))}</span>
+                </td>
+              </tr>
+
+              <tr>
+                <td colSpan={2}>
+                  <b>Total Commission</b>
+                </td>
+
+                <td colSpan={2}>
+                  <span>{numberWithCommas(getTotalCommission(filteredInvoices))}</span>
+                </td>
+              </tr>
+
+
+              <tr>
+                <td>&nbsp;</td>
+              </tr>
+
+              <tr>
+                <td colSpan={2}>
+                  <b>Net Cash</b>
+                </td>
+
+                <td colSpan={2}>
+                  <span>0</span>
+                </td>
+              </tr>
+
+
+              <tr>
+                <td>&nbsp;</td>
+              </tr>
             </tbody>
-        </table>
-      </section>}
+          </table>
+        </section>
+
+
       </div>
+
     </>
   );
 }
